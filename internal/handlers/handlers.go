7d8da5d9b39379ecc0875db9d6b77c710fb0938a
Tagir Gumerov <tigprog@gmail.com>
2022-07-22 16:32:50 +0300

    [GOHW-1]: update handlers 1

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"fmt"
 	"log"
+	"strconv"
 	"strings"
 
 	"github.com/pkg/errors"
@@ -25,30 +26,37 @@ func listFunc(s string) string {
 	for _, v := range data {
 		res = append(res, v.String())
 	}
+	if len(res) == 0 {
+		return "<empty list>"
+	}
 	return strings.Join(res, "\n")
 }
 
 func helpFunc(s string) string {
 	return "/help - list commands\n" +
 		"/list - list data\n" +
-		"/add <name> <password> - add new user with name and password"
+		"/add <route> <date> <seat> - add new bus booking with route, date and seat"
 }
 
 func addFunc(data string) string {
 	log.Printf("add command param: <data>")
 	params := strings.Split(data, " ")
-	if len(params) != 2 {
+	if len(params) != 3 {
 		return errors.Wrapf(BadArgument, "%d items: <%v>", len(params), params).Error()
 	}
-	u, err := storage.NewUser(params[0], params[1])
+	seatUint64, err := strconv.ParseUint(params[2], 10, 32)
 	if err != nil {
 		return err.Error()
 	}
-	err = storage.Add(u)
+	bb, err := storage.NewBusBooking(params[0], params[1], uint(seatUint64))
 	if err != nil {
 		return err.Error()
 	}
-	return fmt.Sprintf("user %v added", u)
+	err = storage.Add(bb)
+	if err != nil {
+		return err.Error()
+	}
+	return fmt.Sprintf("bus booking %v added", bb)
 }
 
 func AddHandlers(c *commander.Commander) {
