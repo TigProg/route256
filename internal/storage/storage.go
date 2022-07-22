9f976cc0d1ef28ffc30b2e62b710b1474c483f03
Tagir Gumerov <tigprog@gmail.com>
2022-07-22 16:30:18 +0300

    [GOHW-1]: update storage 1

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -7,48 +7,48 @@ import (
 	"github.com/pkg/errors"
 )
 
-var data map[uint]*User
+var data map[uint]*BusBooking
 
-var UserNotExists = errors.New("user dows not exist")
-var UserExists = errors.New("user exists")
+//var BusBookingNotExists = errors.New("bus booking dows not exist")
+var BusBookingExists = errors.New("bus booking exists")
 
 func init() {
 	log.Println("init storage")
-	data = make(map[uint]*User)
-	u, _ := NewUser("Kirill", "123456")
-	if err := Add(u); err != nil {
+	data = make(map[uint]*BusBooking)
+	bb, _ := NewBusBooking("ABCD45", "2022-01-01", uint(7))
+	if err := Add(bb); err != nil {
 		log.Panic(err)
 	}
 }
 
-func List() []*User {
-	res := make([]*User, 0, len(data))
+func List() []*BusBooking {
+	res := make([]*BusBooking, 0, len(data))
 	for _, v := range data {
 		res = append(res, v)
 	}
 	return res
 }
 
-func Add(u *User) error {
-	if _, ok := data[u.GetId()]; ok {
-		return errors.Wrap(UserExists, strconv.FormatUint(uint64(u.GetId()), 10))
+func Add(bb *BusBooking) error {
+	if _, ok := data[bb.GetId()]; ok {
+		return errors.Wrap(BusBookingExists, strconv.FormatUint(uint64(bb.GetId()), 10))
 	}
-	data[u.GetId()] = u
+	data[bb.GetId()] = bb
 	return nil
 }
 
-func Update(u *User) error {
-	if _, ok := data[u.GetId()]; !ok {
-		return errors.Wrap(UserNotExists, strconv.FormatUint(uint64(u.GetId()), 10))
-	}
-	data[u.GetId()] = u
-	return nil
-}
-
-func Delete(id uint) error {
-	if _, ok := data[id]; ok {
-		delete(data, id)
-		return nil
-	}
-	return errors.Wrap(UserNotExists, strconv.FormatUint(uint64(id), 10))
-}
+//func Update(u *User) error {
+//	if _, ok := data[u.GetId()]; !ok {
+//		return errors.Wrap(UserNotExists, strconv.FormatUint(uint64(u.GetId()), 10))
+//	}
+//	data[u.GetId()] = u
+//	return nil
+//}
+//
+//func Delete(id uint) error {
+//	if _, ok := data[id]; ok {
+//		delete(data, id)
+//		return nil
+//	}
+//	return errors.Wrap(UserNotExists, strconv.FormatUint(uint64(id), 10))
+//}
