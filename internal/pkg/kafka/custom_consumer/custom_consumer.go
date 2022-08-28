e1c2e851ff4deff1fd6b842b34b0ed41a58a46b4
Tagir Gumerov <tigprog@gmail.com>
2022-08-29 02:50:41 +0300

    [GOHW-6]: fix consumer

--- internal/pkg/kafka/custom_consumer/custom_consumer.go
+++ internal/pkg/kafka/custom_consumer/custom_consumer.go
@@ -1,13 +1,16 @@
-package custom_kafka_consumer
+package custom_consumer
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"log"
 	"time"
 
 	"github.com/Shopify/sarama"
 	configPkg "gitlab.ozon.dev/tigprog/bus_booking/internal/config"
 	repoPkg "gitlab.ozon.dev/tigprog/bus_booking/internal/pkg/core/bus_booking/repository"
+	kafkaPkg "gitlab.ozon.dev/tigprog/bus_booking/internal/pkg/kafka"
 )
 
 func New(brokers []string, offsetInitial int64, isReturnErrors bool, repo repoPkg.Interface) (sarama.ConsumerGroupHandler, error) {
@@ -53,7 +56,10 @@ func (c *Consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim saram
 				return nil
 			} else {
 				log.Printf("%v", msg.Value)
-				// TODO
+				err := c.handle(msg.Value)
+				if err != nil {
+					// TODO
+				}
 				session.MarkMessage(msg, "")
 			}
 		}
@@ -67,5 +73,51 @@ func (c *Consumer) Run(ctx context.Context, topics []string, consumerSleep time.
 			log.Printf("on consume: %v", err)
 			time.Sleep(consumerSleep)
 		}
+	}
+}
+
+func (c *Consumer) handle(value []byte) error {
+	commonMsg := kafkaPkg.CommonMessage{}
+	err := json.Unmarshal(value, &commonMsg)
+	if err != nil {
+		return err // TODO custom error
+	}
+
+	specificMsg, err := commonMsg.ToSpecificMessage()
+	if err != nil {
+		return err // TODO custom error
+	}
+
+	ctx := context.Background() // TODO
+
+	// TODO add single handler
+	switch commonMsg.Key {
+	case "add":
+		addMsg := specificMsg.(kafkaPkg.AddMessage)
+		_, err := c.repo.Add(ctx, addMsg.Bb)
+		return err
+	case "change_seat":
+		changeSeatMsg := specificMsg.(kafkaPkg.ChangeSeatMessage)
+		return c.repo.ChangeSeat(
+			ctx,
+			changeSeatMsg.Id,
+			changeSeatMsg.NewSeat,
+		)
+	case "change_date_seat":
+		changeDateSeatMsg := specificMsg.(kafkaPkg.ChangeDateSeatMessage)
+		return c.repo.ChangeDateSeat(
+			ctx,
+			changeDateSeatMsg.Id,
+			changeDateSeatMsg.NewDate,
+			changeDateSeatMsg.NewSeat,
+		)
+	case "delete":
+		deleteMsg := specificMsg.(kafkaPkg.DeleteMessage)
+		return c.repo.Delete(
+			ctx,
+			deleteMsg.Id,
+		)
+	default:
+		return errors.New("unreachable code") // TODO
 	}
 }
